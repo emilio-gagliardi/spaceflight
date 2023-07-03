ac10b002e804fdfbf45645b2d8a3ceb9aa234bf9
Emilio Gagliardi <emilio.gagliardi@gmail.com>
2023-07-03 21:42:54 +0000

    updated data_science.yml with target

--- src/spaceflights/pipelines/data_science/nodes.py
+++ src/spaceflights/pipelines/data_science/nodes.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-
+logger = logging.getLogger(__name__)
 
 def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
     """Splits data into features and targets training and test sets.
@@ -16,8 +16,10 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
     Returns:
         Split data.
     """
+    
     X = data[parameters["features"]]
-    y = data["price"]
+    logger.info(f"what is being passed via params.model_parameters.target: {parameters['target']}")
+    y = data[parameters["target"]]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=parameters["test_size"], random_state=parameters["random_state"]
     )
@@ -51,5 +53,4 @@ def evaluate_model(
     """
     y_pred = regressor.predict(X_test)
     score = r2_score(y_test, y_pred)
-    logger = logging.getLogger(__name__)
     logger.info("Model has a coefficient R^2 of %.3f on test data.", score)
